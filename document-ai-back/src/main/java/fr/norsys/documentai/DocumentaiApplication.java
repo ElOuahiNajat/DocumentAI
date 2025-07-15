83b448732804d2b050cd51f30657e664ac4f57ec
aalmai <aalmai@norsys.fr>
2025-07-15 16:06:16 +0100

    refactor(api): jpa auditing config

--- document-ai-back/src/main/java/fr/norsys/documentai/DocumentaiApplication.java
+++ document-ai-back/src/main/java/fr/norsys/documentai/DocumentaiApplication.java
@@ -2,10 +2,8 @@ package fr.norsys.documentai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DocumentaiApplication {
 
 	public static void main(String[] args) {
---
+++ document-ai-back/src/main/java/fr/norsys/documentai/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package fr.norsys.documentai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}

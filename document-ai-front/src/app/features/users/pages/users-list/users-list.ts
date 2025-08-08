959fdff79b3993ed6fa956271081c677bd126fa8
Wissal ENNAJAH ENNAJAH <najah.wiss20@gmail.com>
2025-08-08 10:49:56 +0000

    features(front-41)/ListingUsers

--- document-ai-front/src/app/features/users/pages/users-list/users-list.ts
+++ document-ai-front/src/app/features/users/pages/users-list/users-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {UserList} from '../../components/user-list/user-list';
 
 @Component({
   selector: 'users-users-list',
-  imports: [],
+  imports: [UserList],
   templateUrl: './users-list.html',
 })
 export class UsersList {
